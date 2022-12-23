58e93588454b997f6930f05b5ceac402cbfba176
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-23 17:47:48 -0500

    Create base functionality for user input and writing file

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# readme-generator
-A README file generator
+# projecto
+
+## Description
+
+it projects
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+install the project
+
+## Usage
+
+step 1. do it
+
+## Credits
+
+exzilium
+caseyjdwyer@gmail.com
+
+## License
+
+B
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## How to Contribute
+
+casey
+
+## Tests
+
+undefined
--- index.js
+++ index.js
@@ -2,62 +2,119 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-// title of project, description, table of contents(?), installation, usage, license, contributing, tests, questions
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "input",
-    name: "desc",
-    message: "Please provide a description of your project.",
-  },
-  {
-    type: "input",
-    name: "install",
-    message: "Provide installation instructions",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "How can the app be used?",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Which license does your project use?",
-    choices: ["A", "B", "C"],
-  },
-  {
-    type: "input",
-    name: "contribute",
-    message: "Would you like to list any contributors?",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "How can the app be tested?",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Provide a github username as a good contact for questions about the app.",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Provide an email address for that contact.",
-  },
-];
+// function to use inquirer to prompt user response and write them to a README.md file
+function writeToFile() {
+  inquirer
+    // user prompts
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+      },
+      {
+        type: "input",
+        name: "desc",
+        message: "Please provide a description of your project.",
+      },
+      {
+        type: "input",
+        name: "install",
+        message: "Provide installation instructions",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "How can the app be used?",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Which license does your project use?",
+        choices: ["A", "B", "C"],
+      },
+      {
+        type: "input",
+        name: "contribute",
+        message: "Would you like to list any contributors?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "How can the app be tested?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message:
+          "Provide a github username as a good contact for questions about the app.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Provide an email address for that contact.",
+      },
+    ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    // wait for responses
+    .then((response) => {
+      // template for README contents
+      readmeContent = `
+      # ${response.title}
+
+## Description
+
+${response.desc}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${response.install}
+
+## Usage
+
+${response.usage}
+
+## Credits
+
+${response.github}
+${response.email}
+
+## License
+
+${response.license}
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## How to Contribute
+
+${response.contribute}
+
+## Tests
+
+${response.test}
+`;
+      // write file using template
+      fs.writeFile("README.md", readmeContent, (err) =>
+        err ? console.log(err) : console.log("Success")
+      );
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  writeToFile();
+}
 // Function call to initialize app
 init();
